a1423af1b3a941114d16e6f8fea68d35485873e2
Ranjit Lingaiah <ranjitlingaiah@mbp-1156.local>
2020-02-28 09:51:05 -0800

    Format the code

--- src/main/scala/com/hpe/qss/SecurityLogAnalytics.scala
+++ src/main/scala/com/hpe/qss/SecurityLogAnalytics.scala
@@ -5,8 +5,6 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 
-
-
 object SecurityLogAnalytics {
   def main(args: Array[String]) = {
 
@@ -15,16 +13,39 @@ object SecurityLogAnalytics {
       .getOrCreate()
 
 
-
     import spark.implicits._
 
-    val schema = new StructType().add("received_at", TimestampType).add("message", StringType).add("path", StringType).add("@timestamp", TimestampType).add("auth_status", StringType).add("type", StringType).add("audit_type", StringType).add("audit_sshd_ip", StringType).add("@version", StringType).add("msg", StringType).add("host", StringType).add("terminal", StringType).add("received_from", StringType)
+    val schema = new StructType()
+      .add("received_at", TimestampType)
+      .add("message", StringType)
+      .add("path", StringType)
+      .add("@timestamp", TimestampType)
+      .add("auth_status", StringType)
+      .add("type", StringType)
+      .add("audit_type", StringType)
+      .add("audit_sshd_ip", StringType)
+      .add("@version", StringType)
+      .add("msg", StringType)
+      .add("host", StringType)
+      .add("terminal", StringType)
+      .add("received_from", StringType)
 
-    val lines = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost").option("subscribePattern", "/tmp/stream:audit_log").load().select($"value" cast "string" as "json").select(from_json($"json", schema) as "data").select("data.received_at", "data.auth_status", "data.audit_sshd_ip","data.msg", "data.received_from")
+    val lines = spark.readStream.format("kafka")
+      .option("kafka.bootstrap.servers", "localhost")
+      .option("subscribePattern", "/tmp/stream:audit_log")
+      .load()
+      .select($"value" cast "string" as "json")
+      .select(from_json($"json", schema) as "data")
+      .select("data.received_at", "data.auth_status",
+        "data.audit_sshd_ip", "data.msg", "data.received_from")
 
     val filteredLines = lines.filter($"audit_sshd_ip".isNotNull) //filter out rows that don't have source IP
 
-    val windowedCounts = filteredLines.groupBy(window($"received_at", s"10 seconds", s"5 seconds"), $"audit_sshd_ip").count().orderBy("window")
+    val windowedCounts = filteredLines.groupBy(
+      window($"received_at", s"10 seconds", s"5 seconds"),
+      $"audit_sshd_ip")
+      .count()
+      .orderBy("window")
 
     val query = windowedCounts.writeStream.outputMode("complete").format("console").start().awaitTermination
   }
